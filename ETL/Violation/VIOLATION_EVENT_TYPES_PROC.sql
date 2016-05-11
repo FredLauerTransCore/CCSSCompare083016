0ea1adcc968d3c66872c37486aa08ea41f044eb7
trandccss <david.tran@transcore.com>
2016-05-11 11:55:51 -0400

    Updated version

--- ETL/Violation/VIOLATION_EVENT_TYPES_PROC.sql
+++ ETL/Violation/VIOLATION_EVENT_TYPES_PROC.sql
@@ -1,7 +1,7 @@
 /********************************************************
 *
 * Name: VIOLATION_EVENT_TYPES_PROC
-* Created by: DT, 4/8/2016
+* Created by: DT, 4/11/2016
 * Revision: 1.0
 * Description: This is the template for bulk read/write
 *              VIOLATION_EVENT_TYPES
@@ -9,22 +9,16 @@
 ********************************************************/
 
 CREATE OR REPLACE PROCEDURE VIOLATION_EVENT_TYPES_PROC IS
-
-TYPE VIOLATION_EVENT_TYPESTyp IS TABLE OF VIOLATION_EVENT_TYPES%ROWTYPE 
-     INDEX BY BINARY_INTEGER;
-VIOLATION_EVENT_TYPES_tab VIOLATION_EVENT_TYPESTyp;
-
 l_VIOLATION_EVENT_TYPE_ID dbms_sql.NUMBER_table;
 l_STATE_MODEL_ID dbms_sql.NUMBER_table;
 l_CODE dbms_sql.VARCHAR2_table;
 l_DESCRIPTION dbms_sql.VARCHAR2_table;
 l_LABEL dbms_sql.VARCHAR2_table;
+
 p_count dbms_sql.number_table;
 P_ARRAY_SIZE NUMBER:=10000;
 
-/* Change the FTE_SOURCE_TABLE to the actual table              */
-
-CURSOR C1 IS SELECT * FROM patron.VIOLATION_EVENT_TYPES@mysuntoll;
+CURSOR C1 IS SELECT * FROM FTE_SOURCE_TABLE; /* replace FTE_SOURCE_TABLE to actual table*/
 
 BEGIN
 
@@ -40,6 +34,9 @@ BEGIN
   ,l_DESCRIPTION
   ,l_LABEL
   LIMIT P_ARRAY_SIZE;
+
+  /*Bulk transform, l_VIOLATION_EVENT_TYPE_ID.COUNT is the upper limit of the loop */
+
 
   /*Bulk insert */                           
   FORALL I IN 1 .. l_VIOLATION_EVENT_TYPE_ID.COUNT
@@ -59,13 +56,20 @@ BEGIN
     ) 
       ;
     EXIT WHEN C1%NOTFOUND;
+
+    /*Summary, load status, process status update */
+
+    COMMIT;
+
   END LOOP;
 
-  COMMIT;
 
   CLOSE C1;
 
+  /*Summary, load status, process status update */
+
   COMMIT;
+
 END;
 /
 SHOW ERRORS
