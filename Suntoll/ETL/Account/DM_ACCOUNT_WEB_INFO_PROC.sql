72065adda8238fca7fca7db8c67b87ae4145a69f
headrccss <rebecca.head@transcore.com>
2016-05-20 11:24:53 -0400

    New and Updated DM target table procedures

---
+++ Suntoll/ETL/Account/DM_ACCOUNT_WEB_INFO_PROC.sql
@@ -0,0 +1,89 @@
+/********************************************************
+*
+* Name: DM_ACCOUNT_WEB_INFO_PROC
+* Created by: DT, 4/13/2016
+* Revision: 1.0
+* Description: This is the template for bulk read/write
+*              DM_ACCOUNT_WEB_INFO
+*
+********************************************************/
+
+set serveroutput on
+set verify on
+set echo on
+
+CREATE OR REPLACE PROCEDURE DM_ACCOUNT_WEB_INFO_PROC IS
+
+TYPE DM_ACCOUNT_WEB_INFO_TYP IS TABLE OF DM_ACCOUNT_WEB_INFO%ROWTYPE 
+     INDEX BY BINARY_INTEGER;
+DM_ACCOUNT_WEB_INFO_tab DM_ACCOUNT_WEB_INFO_TYP;
+
+
+P_ARRAY_SIZE NUMBER:=10000;
+
+-- EXTRACT RULE 
+-- Join id of KS_USER to user_id of KS_USER_PA_ACCT_ASSOC to get external user acct_num
+
+CURSOR C1 IS SELECT 
+--    ETC_ACCOUNT_ID ETC_ACCOUNT_ID
+    ua.PA_ACCT_NUM ETC_ACCOUNT_ID
+    ,u.USERNAME USER_NAME
+    ,u.PASSWORD PASSWORD  -- (Encrypted) How?
+    ,u.LAST_LOGIN_DATE LAST_LOGIN_DATETIME
+    ,u.ACCT_LOCKED_UNTIL_DATE INVALID_LOGIN_DATETIME
+    ,u.LOGIN_ATTEMPT_COUNT INVALID_LOGIN_COUNT
+    ,decode(u.ACCT_LOCKED_UNTIL_DATE,'Y','N') PASSWORD_RESET  --Derived- IF INVALID LOGIN DATETIME IS NOT NULL THEN YES ELSE NO
+    ,NULL UPDATE_TS
+    ,NULL LAST_LOGOUT_DATETIME
+    ,NULL LOGIN_IP_ADDRESS  -- LOGIN_IP ADDRESS of MAX(LOGIN_DATE) from PA_WEB_LOGIN_INFO
+    ,NULL USER_AGENT
+--    ,ic.ANI ANI
+--    ,ic.START_TIME IVR_CALL_START_TIME
+--    ,ic.END_TIME IVR_CALL_END_TIME
+    ,NULL LAST_IVR_CALL_DATE  -- SELECT MAX(LAST_LOGIN_DATE) from PA_WEB_LOGIN_INFO WHERE USER AGENT = 'IVR'
+    ,'SUNTOLL' SOURCE_SYSTEM
+FROM KS_USER_PA_ACCT_ASSOC ua
+    ,KS_USER u
+--    ,PA_WEB_LOGIN_INFO wl
+--    ,IVR_CALL ic
+WHERE ua.USER_ID = u.ID
+--AND   ua.
+--AND   ua.PA_ACCT_NUM = wi.ACCT_NUM (+)
+; -- Source SunToll
+
+BEGIN
+ 
+  OPEN C1;  
+
+  LOOP
+
+    /*Bulk select */
+    FETCH C1 BULK COLLECT INTO DM_ACCOUNT_WEB_INFO_tab
+    LIMIT P_ARRAY_SIZE;
+
+    /*ETL SECTION BEGIN
+
+      ETL SECTION END*/
+
+    /*Bulk insert */ 
+    FORALL i in DM_ACCOUNT_WEB_INFO_tab.first .. DM_ACCOUNT_WEB_INFO_tab.last
+           INSERT INTO DM_ACCOUNT_WEB_INFO VALUES DM_ACCOUNT_WEB_INFO_tab(i);
+                       
+    EXIT WHEN C1%NOTFOUND;
+  END LOOP;
+
+  COMMIT;
+
+  CLOSE C1;
+
+  COMMIT;
+
+  EXCEPTION
+  WHEN OTHERS THEN
+     DBMS_OUTPUT.PUT_LINE('ERROR CODE: '||SQLCODE);
+     DBMS_OUTPUT.PUT_LINE('ERROR MSG: '||SQLERRM);
+END;
+/
+SHOW ERRORS
+
+
--- Suntoll/ETL/Transactions/DM_INVOICE_ITM_INFO_PROC.sql
+++ Suntoll/ETL/Transactions/DM_INVOICE_ITM_INFO_PROC.sql
@@ -25,17 +25,17 @@ P_ARRAY_SIZE NUMBER:=10000;
 CURSOR C1 IS SELECT 
     ACCT_NUM ACCOUNT_NUMBER -- ST_DOCUMENT_MAILING_EXCEPTION
     ,DOCUMENT_ID INVOICE_NUMBER
-    ,'Postpaid' CATEGORY  -- DEFAULT to 'Postpaid'
+    ,'POSTPAID' CATEGORY  -- DEFAULT to 'Postpaid'
     ,NULL SUB_CATEGORY
     ,NULL STATUS
-    ,NULL INVOICE_DATE
+    ,CREATED_ON INVOICE_DATE
     ,NULL PAYABLE
     ,NULL INVOICE_ITEM_NUMBER
     ,NULL LANE_TX_ID
     ,NULL LEVEL_INFO
-    ,NULL REASON_CODE
-    ,NULL INVOICE_TYPE
-    ,NULL PAID_AMOUNT
+    ,PROMOTION_CODE REASON_CODE
+    ,'INVOICE-ITEM' INVOICE_TYPE
+    ,0 PAID_AMOUNT
     ,NULL CREATED
     ,NULL CREATED_BY
     ,NULL LAST_UPD
@@ -78,5 +78,66 @@ BEGIN
 END;
 /
 SHOW ERRORS
+
+
+
+------------------  Work sheet for 
+
+OTHER (balance adjustments) - FOR VB_ACTIVITY KS_LEDGER_EVENT_TYPE_ID in (‘8’) and COLL_COURT_FLAG is null and Bankruptcy_flag is null JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w DOCUMENT_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID in (‘1’,’2’)
+
+INVTOLL
+FOR VB_ACTIVITY KS_LEDGER_EVENT_TYPE_ID in (‘38’,’42’) and COLL_COURT_FLAG is null and Bankruptcy_flag is null JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w DOCUMENT_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID in (‘1’,’2’)
+ESCTOLL
+FOR VB_ACTIVITY KS_LEDGER_EVENT_TYPE_ID in (‘38’,’42’) and COLL_COURT_FLAG is null and Bankruptcy_flag is null JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w CHILD_DOC_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID in (‘1’,’2’)
+CLTOLL (collection toll)
+VB_ACTIVITY KS_LEDGER_EVENT_TYPE_ID in (‘38’,’42’) and COLL_COURT_FLAG = ‘COLL’ and Bankruptcy_flag is null. JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w CHILD_DOC_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID in (‘1’,’2’) gives latest document  (Collection assignment is not associated to a document creation )
+UTCTOLL
+FOR VB_ACTIVITY KS_LEDGER_EVENT_TYPE_ID in (‘38’,’42’) and COLL_COURT_FLAG is null and Bankruptcy_flag is null JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w CHILD_DOC_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID =’3’
+COURTTOLL
+VB_ACTIVITY KS_LEDGER_EVENT_TYPE_ID in (‘38’,’42’) and COLL_COURT_FLAG = ‘CRT’ and Bankruptcy_flag is null. JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w CHILD_DOC_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID in (‘1’,’2’) gives latest document  (Court assignment is not associated to a document creation )
+COURTPLEATOLL
+Join VB_ACTIVITY.KS_LEDGER_EVENT_TYPE_ID =’89’ and COLL_COURT_FLAG is null and Bankruptcy_flag is null. JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w DOCUMENT_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID in (‘1’,’2’) 
+REGHOLDTOLL
+Join ST_ACCT_FLAGS.ACCT_NUM = ST_document_info.acct_num and ST_ACCT_FLAG.FLAG_TYPE =’9’ and END_DATE is null, All the following tolls are on REG HOLD
+JOIN ID of KS_LEDGER to LEDGER_ID of VB_ACTVITY for KS_LEDGER.TRANSACTION_TYPE in ('38','42') and JOIN PA_LANE_TXN_ID of KS_LEDGER to TXN_ID of PA_LANE_LANE_TXN and JOIN PLAZA_ID of PA_PLAZA to EXT_PLAZA_ID for PA_Lane_txn
+  IF SUM (VB_ACTIVITY.AMT_CHARGED – VB_ACTIVITY.TOTAL_AMT_PAID) < :REG_STOP_THRESHOLD_AMT_IN_CENTS grouped by KS_LEDGER.ACCT_NUM
+  FOR PA_PLAZA.FTE_PLAZA = 'Y',VB_ACTIVITY.COLL_COURT_FLAG is NULL, VB_ACTIVITY.CHILD_DOC_ID not null,VB_ACTIVITY.CHILD_DOC_ID NOT LIKE '%-%' and VB_ACTIVITY.BANKRUPTCY_flag is null
+  THEN 'REG STOP'
+ADMINFEE
+FOR VB_ACTIVITY KS_LEDGER_EVENT_TYPE_ID in (‘48’) and COLL_COURT_FLAG is null and Bankruptcy_flag is null JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w DOCUMENT_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID in (‘1’,’2’)
+ESCADMINFEE
+FOR VB_ACTIVITY KS_LEDGER_EVENT_TYPE_ID in (‘48’) and COLL_COURT_FLAG is null and Bankruptcy_flag is null JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w CHILD_DOC_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID in (‘1’,’2’)
+CLADMINFEE
+VB_ACTIVITY KS_LEDGER_EVENT_TYPE_ID in (‘48’) and COLL_COURT_FLAG = ‘COLL’ and Bankruptcy_flag is null. JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w CHILD_DOC_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID in (‘1’,’2’) gives latest document  (Collection assignment is not associated to a document creation )
+UTCFEE
+FOR VB_ACTIVITY KS_LEDGER_EVENT_TYPE_ID in (‘25’) and COLL_COURT_FLAG is null and Bankruptcy_flag is null JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w DOCUMENT_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID =’3’
+CRTPLEAFEE
+Join VB_ACTIVITY.KS_LEDGER_EVENT_TYPE_ID =’100’ and COLL_COURT_FLAG is null and Bankruptcy_flag is null. JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w DOCUMENT_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID in (‘1’,’2’)
+COURTUTCFEE
+VB_ACTIVITY KS_LEDGER_EVENT_TYPE_ID in (‘25’) and COLL_COURT_FLAG = ‘CRT’ and Bankruptcy_flag is null. JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w CHILD_DOC_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID in (‘1’,’2’) gives latest document  (Court assignment is not associated to a document creation )
+EXPLANECHARGE (express lane charge)
+FOR VB_ACTIVITY KS_LEDGER_EVENT_TYPE_ID in (‘145’) and COLL_COURT_FLAG is null and Bankruptcy_flag is null JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w DOCUMENT_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID in (‘1’,’2’)
+ESCEXPPLANCECHARGE
+FOR VB_ACTIVITY KS_LEDGER_EVENT_TYPE_ID in (‘145’) and COLL_COURT_FLAG is null and Bankruptcy_flag is null JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w CHILD_DOC_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID in (‘1’,’2’)
+
+MAILFEE (include a statement fee in case this is in the data)- We do not do this currently
+
+NSF
+FOR VB_ACTIVITY KS_LEDGER_EVENT_TYPE_ID in (‘17’) and COLL_COURT_FLAG is null and Bankruptcy_flag is null JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w DOCUMENT_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID in (‘1’,’2’)
+ESCNSF
+FOR VB_ACTIVITY KS_LEDGER_EVENT_TYPE_ID in (‘17’) and COLL_COURT_FLAG is null and Bankruptcy_flag is null JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w CHILD_DOC_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID in (‘1’,’2’)
+
+CLNSF
+VB_ACTIVITY KS_LEDGER_EVENT_TYPE_ID in (‘17’) and COLL_COURT_FLAG = ‘COLL’ and Bankruptcy_flag is null. JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w CHILD_DOC_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID in (‘1’,’2’) gives latest document  (Collection assignment is not associated to a document creation )
+
+
+CLEXPLANECHARGE
+VB_ACTIVITY KS_LEDGER_EVENT_TYPE_ID in (‘145’) and COLL_COURT_FLAG = ‘COLL’ and Bankruptcy_flag is null. JOIN DOCUMENT_ID of ST_DOCUMENT_INFO w CHILD_DOC_ID of VB_ACTIVITY where ST_DOCUMENT_INFO.DOC_TYPE_ID in (‘1’,’2’) gives latest document  (Collection assignment is not associated to a document creation )
+
+
+**Discussed bankruptcy, deceased, court – these will be tracked as statuses – use Bankruptcy flag in VB_Activity to BKTY in above queries to get this status, 
+                                                                                                                                              For ST_document_info. Acct_num joined PA_ACCT_DETAIL.DECEASED_DATE
+                                                                                                                                               CRT is provided above
+
 
 
