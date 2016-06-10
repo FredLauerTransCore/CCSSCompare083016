eb55e85e37f0d2002ed02c94c5307790a284672d
Jared Lapsley <jared.lapsley@transcore.com>
2016-06-09 21:09:05 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/TransCore-FL/ccss""
    This reverts commit 58420e7ab1b2484893353fcfb24d740994055576.

---
+++ Sunpass/ETL/History/DM_ACCT_STATUS_HST_INFO_PROC.sql
@@ -0,0 +1,71 @@
+/********************************************************
+*
+* Name: DM_ACCT_STATUS_HST_INFO_PROC
+* Created by: DT, 5/8/2016
+* Revision: 1.0
+* Description: This is the template for bulk read/write
+*              DM_ACCT_STATUS_HST_INFO
+*
+********************************************************/
+
+set serveroutput on
+set verify on
+set echo on
+
+CREATE OR REPLACE PROCEDURE DM_ACCT_STATUS_HST_INFO_PROC IS
+
+TYPE DM_ACCT_STATUS_HST_INFO_TYP IS TABLE OF DM_ACCT_STATUS_HST_INFO%ROWTYPE 
+     INDEX BY BINARY_INTEGER;
+DM_ACCT_STATUS_HST_INFO_tab DM_ACCT_STATUS_HST_INFO_TYP;
+
+
+P_ARRAY_SIZE NUMBER:=10000;
+
+
+CURSOR C1 IS SELECT 
+    PREV_STATUS PREV_STATUS
+    ,NEW_STATUS NEW_STATUS
+    ,STAT_CHANGE_DATE STAT_CHANGE_DATE
+    ,STAT_POST_DATE STAT_POST_DATE
+    ,EMP_ID EMP_ID
+    ,ACCT_NUM ACCT_NUM
+    ,'SUNPASS' SOURCE_SYSTEM
+FROM PA_ACCT_STATUS_HISTORY;
+
+BEGIN
+ 
+  OPEN C1;  
+
+  LOOP
+
+    /*Bulk select */
+    FETCH C1 BULK COLLECT INTO DM_ACCT_STATUS_HST_INFO_tab
+    LIMIT P_ARRAY_SIZE;
+
+
+    /*ETL SECTION BEGIN */
+
+    /*ETL SECTION END   */
+
+    /*Bulk insert */ 
+    FORALL i in DM_ACCT_STATUS_HST_INFO_tab.first .. DM_ACCT_STATUS_HST_INFO_tab.last
+           INSERT INTO DM_ACCT_STATUS_HST_INFO VALUES DM_ACCT_STATUS_HST_INFO_tab(i);
+                       
+    EXIT WHEN C1%NOTFOUND;
+  END LOOP;
+
+  COMMIT;
+
+  CLOSE C1;
+
+  COMMIT;
+
+  EXCEPTION
+  WHEN OTHERS THEN
+     DBMS_OUTPUT.PUT_LINE('ERROR CODE: '||SQLCODE);
+     DBMS_OUTPUT.PUT_LINE('ERROR MSG: '||SQLERRM);
+END;
+/
+SHOW ERRORS
+
+
--- Suntoll/ETL/Account/DM_NONFIN_ACT_INFO_PROC.sql
+++ Suntoll/ETL/Account/DM_NONFIN_ACT_INFO_PROC.sql
@@ -1,7 +1,7 @@
 /********************************************************
 *
 * Name: DM_NONFIN_ACT_INFO_PROC
-* Created by: RH, 4/17/2016
+* Created by: RH, 6/4/2016
 * Revision: 1.0
 * Description: This is the template for bulk read/write
 *              DM_NONFIN_ACT_INFO
@@ -12,7 +12,10 @@ set serveroutput on
 set verify on
 set echo on
 
-CREATE OR REPLACE PROCEDURE DM_NONFIN_ACT_INFO_PROC IS
+-- EMP_CODE IS LIKE '99xx' or '0100' -- SYSTEM
+
+declare
+--CREATE OR REPLACE PROCEDURE DM_NONFIN_ACT_INFO_PROC IS
 
 TYPE DM_NONFIN_ACT_INFO_TYP IS TABLE OF DM_NONFIN_ACT_INFO%ROWTYPE 
      INDEX BY BINARY_INTEGER;
@@ -24,19 +27,37 @@ P_ARRAY_SIZE NUMBER:=10000;
 
 CURSOR C1 IS SELECT 
     ACCT_NUM ACCOUNT_NUMBER
-    ,NULL ACTIVITY_NUMBER
-    ,NULL CATEGORY
-    ,NULL SUB_CATEGORY
-    ,NULL ACTIVITY_TYPE
-    ,NULL DESCRIPTION
-    ,NULL CREATED
-    ,NULL CREATED_BY
-    ,NULL LAST_UPD
-    ,NULL LAST_UPD_BY
+    ,nvl((SELECT MAX(ACTIVITY_NUMBER + 1) 
+        FROM DM_NONFIN_ACT_INFO 
+        WHERE ACCT_NUM = NOTE.ACCT_NUM),0) as ACTIVITY_NUMBER
+--    ,NULL ACTIVITY_NUMBER
+    ,TYPE_CODE CATEGORY
+    ,PROB_CODE SUB_CATEGORY
+    ,TYPE_ID ACTIVITY_TYPE
+    ,NOTE DESCRIPTION
+    ,CREATED CREATED
+    ,EMP_CODE CREATED_BY
+    ,CREATED LAST_UPD
+    ,EMP_CODE LAST_UPD_BY
     ,'SUNTOLL' SOURCE_SYSTEM
-FROM FTE_TABLE; /*Change FTE_TABLE to the actual table name*/
+FROM NOTE
+where EMP_CODE like '99%' or EMP_CODE = '0100'
+and rownum<101
+; 
+--
+
+SQL_STRING  varchar2(500) := 'truncate table ';
+LOAD_TAB    varchar2(50) := 'DM_NONFIN_ACT_INFO';
+ROW_CNT NUMBER := 0;
 
 BEGIN
+  DBMS_OUTPUT.PUT_LINE('Start '||LOAD_TAB||' load at: '||to_char(SYSDATE,'MON-DD-YYYY HH:MM:SS'));
+
+  SQL_STRING := SQL_STRING||LOAD_TAB;
+  DBMS_OUTPUT.PUT_LINE('SQL_STRING : '||SQL_STRING);
+  execute immediate SQL_STRING;
+  commit;
+
  
   OPEN C1;  
 
@@ -55,6 +76,10 @@ BEGIN
     FORALL i in DM_NONFIN_ACT_INFO_tab.first .. DM_NONFIN_ACT_INFO_tab.last
            INSERT INTO DM_NONFIN_ACT_INFO VALUES DM_NONFIN_ACT_INFO_tab(i);
                        
+--    DBMS_OUTPUT.PUT_LINE('Inserted '||sql%rowcount||' into '||LOAD_TAB||' at: '||to_char(SYSDATE,'MON-DD-YYYY HH:MM:SS'));
+    ROW_CNT := ROW_CNT +  sql%rowcount;                 
+    DBMS_OUTPUT.PUT_LINE('Inserted ROW count : '||ROW_CNT);
+                       
     EXIT WHEN C1%NOTFOUND;
   END LOOP;
 
@@ -63,6 +88,8 @@ BEGIN
   CLOSE C1;
 
   COMMIT;
+  DBMS_OUTPUT.PUT_LINE('End '||LOAD_TAB||' load at: '||to_char(SYSDATE,'MON-DD-YYYY HH:MM:SS'));
+  DBMS_OUTPUT.PUT_LINE('Total Rows : '||ROW_CNT);
 
   EXCEPTION
   WHEN OTHERS THEN
