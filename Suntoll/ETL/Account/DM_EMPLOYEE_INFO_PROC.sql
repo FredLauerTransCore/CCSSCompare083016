ef2369d02b5df15d5e346b740e299190513770c2
Rebecca Head <rebecca.head@transcore.com>
2016-06-13 16:25:58 -0400

    Adding tracking code for data migration
    Tracking and control modules. Initial release.

--- Suntoll/ETL/Account/DM_EMPLOYEE_INFO_PROC.sql
+++ Suntoll/ETL/Account/DM_EMPLOYEE_INFO_PROC.sql
@@ -13,7 +13,8 @@ set verify on
 set echo on
 
 declare
---CREATE OR REPLACE PROCEDURE DM_EMPLOYEE_INFO_PROC IS
+CREATE OR REPLACE PROCEDURE DM_EMPLOYEE_INFO_PROC (trac_rec  dm_tracking_etl%ROWTYPE);
+IS
 
 TYPE DM_EMPLOYEE_INFO_TYP IS TABLE OF DM_EMPLOYEE_INFO%ROWTYPE 
      INDEX BY BINARY_INTEGER;
@@ -41,53 +42,69 @@ FROM KS_USER
 where rownum<101
 ;   -- Source table SUNTOLL
 
-SQL_STRING  varchar2(500) := 'truncate table ';
-LOAD_TAB    varchar2(50) := 'DM_EMPLOYEE_INFO';
-ROW_CNT NUMBER := 0;
+sql_string  VARCHAR2(500) := 'truncate table ';
+row_cnt NUMBER := 0;
 
 BEGIN
-  DBMS_OUTPUT.PUT_LINE('Start '||LOAD_TAB||' load at: '||to_char(SYSDATE,'MON-DD-YYYY HH:MM:SS'));
+  trac_rec.dm_last_date = SYSDATE;
+  DBMS_OUTPUT.PUT_LINE('Start '||trac_rec.dm_name||' load at: '||to_char(SYSDATE,'MON-DD-YYYY HH:MM:SS'));
+  TRACK_ETL_PROC(trac_rec);
 
-  SQL_STRING := SQL_STRING||LOAD_TAB;
-  DBMS_OUTPUT.PUT_LINE('SQL_STRING : '||SQL_STRING);
-  execute immediate SQL_STRING;
-  commit;
+  sql_string := sql_string||load_tab;
+  dbms_output.put_line('SQL_STRING : '||sql_string);
+  EXECUTE IMMEDIATE sql_string;
+  COMMIT;
 
-  OPEN C1;  
+  OPEN c1;  
+
+  trac_rec.status = 'Processing';
 
   LOOP
-
+    
     /*Bulk select */
-    FETCH C1 BULK COLLECT INTO DM_EMPLOYEE_INFO_tab
-    LIMIT P_ARRAY_SIZE;
+    FETCH C1 BULK COLLECT INTO DM_EMPLOYEE_INFO_TAB
+    LIMIT p_array_size;
 
     /*ETL SECTION BEGIN
+--    trac_rec.status = 'Processing group'||;
 
       ETL SECTION END*/
 
     /*Bulk insert */ 
-    FORALL i in DM_EMPLOYEE_INFO_tab.first .. DM_EMPLOYEE_INFO_tab.last
-           INSERT INTO DM_EMPLOYEE_INFO VALUES DM_EMPLOYEE_INFO_tab(i);
+    FORALL i IN dm_employee_info_tab.FIRST .. dm_employee_info_tab.LAST
+           INSERT INTO dm_employee_info VALUES dm_employee_info_tab(i);
 
---    DBMS_OUTPUT.PUT_LINE('Inserted '||sql%rowcount||' into '||LOAD_TAB||' at: '||to_char(SYSDATE,'MON-DD-YYYY HH:MM:SS'));
-    ROW_CNT := ROW_CNT +  sql%rowcount;                 
-    DBMS_OUTPUT.PUT_LINE('ROW count : '||ROW_CNT);
+    row_cnt := row_cnt +  SQL%ROWCOUNT;
+    trac_rec.dm_load_cnt = row_cnt;
+    trac_rec.dm_last_date = SYSDATE;
+  TRACK_ETL_PROC(trac_rec);
+
     EXIT WHEN C1%NOTFOUND;
   END LOOP;
 
   COMMIT;
 
-  CLOSE C1;
+  CLOSE c1;
 
   COMMIT;
-  DBMS_OUTPUT.PUT_LINE('END '||LOAD_TAB||' load at: '||to_char(SYSDATE,'MON-DD-YYYY HH:MM:SS'));
-  DBMS_OUTPUT.PUT_LINE('Total ROW count : '||ROW_CNT);
+--  dbms_output.put_line('END '||load_tab||' load at: '||to_char(SYSDATE,'MON-DD-YYYY HH:MM:SS'));
+--  dbms_output.put_line('Total ROW count : '||row_cnt);
  -- DBMS_OUTPUT.PUT_LINE('END Count '||LOAD_TAB||' load at: '||to_char(SYSDATE,'MON-DD-YYYY HH:MM:SS'));
+
+    trac_rec.dm_end_date = SYSDATE;
+    trac_rec.status = 'Completed';
+    trac_rec.result_code = SQLCODE;
+    trac_rec.result_msg = SQLERRM;
+    dm_track_proc(trac_rec);
 
   EXCEPTION
   WHEN OTHERS THEN
-     DBMS_OUTPUT.PUT_LINE('ERROR CODE: '||SQLCODE);
-     DBMS_OUTPUT.PUT_LINE('ERROR MSG: '||SQLERRM);
+    trac_rec.result_code = SQLCODE;
+    trac_rec.result_msg = SQLERRM;
+    dm_track_proc(trac_rec);
+     DBMS_OUTPUT.PUT_LINE('ERROR CODE: '||trac_rec.result_code);
+     DBMS_OUTPUT.PUT_LINE('ERROR MSG: '||trac_rec.result_msg);
+    RAISE EXCEPTION;
 END;
 /
 SHOW ERRORS
--- Suntoll/ETL/Account/PRE_SOURCE_PROC.sql
+++ Suntoll/ETL/Account/PRE_SOURCE_PROC.sql
@@ -1,6 +1,6 @@
 /********************************************************
 *
-* Name: PRE_DM_EMPLOYEE_INFO_PROC
+* Name: PRE_SOURCE_PROC
 * Created by: RH, 5/25/2016
 * Revision: 1.0
 * Description: Pre load procedure for DM_EMPLOYEE_INFO
@@ -12,21 +12,23 @@ set verify on
 set echo on
 
 declare
---CREATE OR REPLACE PROCEDURE PRE_DM_EMPLOYEE_INFO_PROC IS
+--
+CREATE OR REPLACE PROCEDURE PRE_SOURCE_PROC (c_rec  IN dm_control%ROWTYPE)
+IS
 
-  TRACK_REC DM_TRACKING%ROWTYPE;
-  ENT_REC   DM_TRACKING_ENTITY%ROWTYPE;
-  TOT_REC   DM_TRACKING_TOTALS%ROWTYPE;
+  Trac_Rec Dm_Tracking%ROWTYPE;
+  tot_rec   dm_tracking_totals%ROWTYPE;
+  ent_rec   dm_tracking_entity%ROWTYPE;
 --TYPE DM_TRACKING_TYP IS TABLE OF DM_TRACKING%ROWTYPE INDEX BY BINARY_INTEGER;
 --DM_EMPLOYEE_INFO_tab DM_EMPLOYEE_INFO_TYP;
 --P_ARRAY_SIZE NUMBER:=1000;
 
-SQL_STRING    varchar2(500) := 'truncate table ';
-DM_LOAD_TAB    varchar2(50) := 'DM_EMPLOYEE_INFO';
-SOURCE_TAB    varchar2(50) := 'KS_USER';
-P_ROW_CNT     NUMBER := 0;
-FROM_CLAUSE    varchar2(100) := 'KS_USER';
-WHERE_CLAUSE    varchar2(500);
+  sql_string    VARCHAR2(500) := 'truncate table ';
+  p_row_cnt     NUMBER := 0;
+  v_select    VARCHAR2(100);
+  v_into    VARCHAR2(100);
+  v_from    VARCHAR2(100);
+  v_where    VARCHAR2(500);
 
 BEGIN
   DBMS_OUTPUT.PUT_LINE('Start '||LOAD_TAB||' load at: '||to_char(SYSDATE,'MON-DD-YYYY HH:MM:SS'));
@@ -36,12 +38,20 @@ BEGIN
 
   TOT_REC.TOTAL_TYPE := 'P_ROW_CNT';
   TOT_REC.TOTAL_NAME := SOURCE_TAB;
-  
-    /*Count Enity */
-  select count(1) into P_ROW_CNT
-  FROM KS_USER  -- source table
-  ;
-  
+
+    V_SELECT := GET_SELECT_CLAUSE(c_rec);
+    V_INTO := GET_INTO_CLAUSE(c_rec);
+    V_FROM := GET_FROM_CLAUSE(c_rec);
+    V_WHERE := GET_WHERE_CLAUSE(c_rec);
+    sql_string := 
+        'SELECT '||V_SELECT
+        ' INTO '||V_INTO
+        ' FROM '||V_FROM
+        ' WHERE '||V_WHERE;
+    DBMS_OUTPUT.PUT_LINE('sql_string : '||sql_string);
+    EXECUTE IMMEDIATE sql_string;
+    execute immediate 'begin :x := ' || vRunFunctie || '( :p1,  :p2, :vParmOut2 ); end;' using out vParmOut1, vParmIn1, vParmIn2, out vParmOut2;
+
   TOT_REC.TOTAL_TYPE := 'P_ROW_CNT';
   TOT_REC.TOTAL_VALUE := P_ROW_CNT;
   TRACK_TOTAL(TRACK_REC, TOT_REC);
